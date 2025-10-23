a8c2757287a503955ffa6166786ddb52d105212c
Roman Piacquadio <roman.piacquadio@cloudx.com>
2025-10-23 19:24:11 -0300

    fixing build errors

--- components/livekit/chat/chat-message-view.tsx
+++ components/livekit/chat/chat-message-view.tsx
@@ -1,15 +1,15 @@
 'use client';
 
-import React, { type RefObject, useRef } from 'react';
+import React from 'react';
 import { cn } from '@/lib/utils';
 
-export function useAutoScroll(scrollContentContainerRef: RefObject<Element | null>) {
+export function useAutoScroll() {
   const scrollToBottom = () => {
     const { scrollingElement } = document;
     if (scrollingElement) {
       scrollingElement.scrollTo({
         top: scrollingElement.scrollHeight,
-        behavior: 'smooth'
+        behavior: 'smooth',
       });
     }
   };
@@ -23,18 +23,23 @@ interface ChatProps extends React.HTMLAttributes<HTMLDivElement> {
 
 export const ChatMessageView = React.forwardRef<{ scrollToBottom: () => void }, ChatProps>(
   ({ className, children, ...props }, ref) => {
-    const scrollContentRef = useRef<HTMLDivElement>(null);
-    const { scrollToBottom } = useAutoScroll(scrollContentRef);
+    const { scrollToBottom } = useAutoScroll();
 
     // Expose scrollToBottom function to parent component
-    React.useImperativeHandle(ref, () => ({
-      scrollToBottom
-    }), [scrollToBottom]);
+    React.useImperativeHandle(
+      ref,
+      () => ({
+        scrollToBottom,
+      }),
+      [scrollToBottom]
+    );
 
     return (
-      <div ref={scrollContentRef} className={cn('flex flex-col justify-end', className)} {...props}>
+      <div className={cn('flex flex-col justify-end', className)} {...props}>
         {children}
       </div>
     );
   }
 );
+
+ChatMessageView.displayName = 'ChatMessageView';
